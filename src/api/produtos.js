69ff326497596c17f386f21997cc12ca75933d1e
Roberta Lima <robertataisaa@gmail.com>
2021-09-13 12:21:54 -0300

    Adicionando Swagger

--- src/api/produtos.js
+++ src/api/produtos.js
@@ -1,56 +1,76 @@
-const express = require('express');
-const { restart } = require('nodemon');
+const express = require("express");
+const { restart } = require("nodemon");
 const router = express.Router();
-const { produtos } = require('../models');
-const ProdutoService = require('../services/produtos');
-const { body, check, validationResult } = require('express-validator');
+const { produtos } = require("../models");
+const ProdutoService = require("../services/produtos");
+const { body, check, validationResult } = require("express-validator");
 
 const produtoService = new ProdutoService(produtos);
 
 /**
  * Obter toda a lista de produtos
  */
-router.get('/', async (req, res) => {
-  try {
-    const produtos = await produtoService.get();
-    res.status(200).json(produtos);
-  } catch (error) {
-    errorResponse(res, error)
-  }
+router.get("/", async (req, res) => {
+  /*
+        #swagger.tags = ['Produtos']
+        #swagger.description = 'Endpoint para obter uma lista de produtos' 
+    
+        #swagger.responses[200] = {
+            schema: { $ref: "#/definitions/Produtos"},
+            description: 'Listagem de produtos.'
+        }
+        #swagger.responses[400] = {
+            description: 'Desculpe, ocorreu um problema.'
+        }
+    */
+
+  const produtos = await produtoService.get();
+  res.status(200).json(produtos);
 });
 
-router.post('/', [
-  body('nome').notEmpty().withMessage('O atributo nome é obrigatório!'),
-  body('preco').notEmpty().isNumeric().withMessage('O atributo preço é obrigatório e deve ser um número decimal!'),
-  body('categoria').notEmpty().withMessage('O atributo categoria é obrigatório!'),
-], async (req, res) => {
-  const erros = validationResult(req)
-  try {
-    if (!erros.isEmpty()) {
-      errorResponse(res, {message: erros.array()}, 400, 'Bad request')
-    } else {
-      const body = req.body
-      const produto = await produtoService.gravar(body)
-      res.status(201).json(produto)
+router.post(
+  "/",
+  [
+    body("nome").notEmpty().withMessage("O atributo nome é obrigatório!"),
+    body("preco")
+      .notEmpty()
+      .isNumeric()
+      .withMessage(
+        "O atributo preço é obrigatório e deve ser um número decimal!"
+      ),
+    body("categoria")
+      .notEmpty()
+      .withMessage("O atributo categoria é obrigatório!"),
+  ],
+  async (req, res) => {
+    const erros = validationResult(req);
+    try {
+      if (!erros.isEmpty()) {
+        errorResponse(res, { message: erros.array() }, 400, "Bad request");
+      } else {
+        const body = req.body;
+        const produto = await produtoService.gravar(body);
+        res.status(201).json(produto);
+      }
+    } catch (e) {
+      errorResponse(res, e);
     }
-  } catch (e) {
-    errorResponse(res, e)
   }
-})
+);
 
 function errorResponse(res, error, statusCode, mensagem) {
-  statusCode = statusCode ? statusCode : 500
-  statusCode = error.statusCode ? error.statusCode : statusCode
+  statusCode = statusCode ? statusCode : 500;
+  statusCode = error.statusCode ? error.statusCode : statusCode;
 
-  mensagem = mensagem ? mensagem : 'Erro no servidor'
-  mensagem = error.mensagem ? error.mensagem : mensagem
+  mensagem = mensagem ? mensagem : "Erro no servidor";
+  mensagem = error.mensagem ? error.mensagem : mensagem;
 
   const resultado = {
     mensagem: mensagem,
-    mensagemErro: error.message
-  }
+    mensagemErro: error.message,
+  };
 
-  res.status(statusCode).json(resultado)
+  res.status(statusCode).json(resultado);
 }
 
 module.exports = router;
--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
-    dialect: 'postgres',
-    host:'localhost',
-    port:5432,
-    database:'GirlsNodePower_db',
-    username: 'postgres',
-    password:'root'
-}
+  dialect: "postgres",
+  host: "localhost",
+  port: 5432,
+  database: "GirlsNodePower_db",
+  username: "postgres",
+  password: "root",
+};
